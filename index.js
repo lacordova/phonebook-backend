8cec79437b3fdc4ab8221780c8bdbedcc5421b3b
lacordova <lacordova@uc.cl>
2021-02-27 02:04:17 -0300

    uibuild

--- index.js
+++ index.js
@@ -14,96 +14,96 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-const handleGetPersons = (req,res,next)=>{
-    Person.find({})
-        .then(persons => {
-          res.json(persons)
-        })
-        .catch(err=>next(err))
+const handleGetPersons = (req,res,next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
+    .catch(err => next(err))
 }
 
 const handleGetNoteId = (req,res, next) => {
-    const id = req.params.id
-    Person.findById(id)
-        .then(person => {
-            res.json(person)
-        })
-        .catch(err => next(err))
+  const id = req.params.id
+  Person.findById(id)
+    .then(person => {
+      res.json(person)
+    })
+    .catch(err => next(err))
 }
 
 const handleInfo = (req, res) => {
-    const getInfo = (err, count)=> {
-        const date = getDate()
-        const templateStr = `
+  const getInfo = (err, count) => {
+    const date = getDate()
+    const templateStr = `
         <p>
             Phonebook has info for ${String(count)} people <br> <br>
             ${date}
         </p>
         `
-        res.send(templateStr)
-    }
-    Person.countDocuments({}, getInfo)
+    res.send(templateStr)
+  }
+  Person.countDocuments({}, getInfo)
 }
 
 const handleDeletePerson = (req, res,next) => {
-    const id = req.params.id
-    Person.findByIdAndDelete(id)
-        .then(()=> {
-            res.status(204).end()
-        })
-        .catch(err=>next(err))
+  const id = req.params.id
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(err => next(err))
 
 }
 
 const handlePostPerson = (req,res, next) => {
-    const body = req.body
+  const body = req.body
 
-    //     return res.status(400).json({ error: 'name must be unique' })}
+  //     return res.status(400).json({ error: 'name must be unique' })}
 
-    const newPerson = new Person({
+  const newPerson = new Person({
 
-        name: body.name,
-        number: body.number,
+    name: body.name,
+    number: body.number,
+  })
+  newPerson.save()
+    .then(person => {
+      res.json(person)
     })
-    newPerson.save()
-        .then(person => {
-            res.json(person)
-        })
-        .catch(err => next(err))
+    .catch(err => next(err))
 }
 
 const handleUpdatePerson = (req, res, next) => {
-    const id = req.params.id
-    const body = req.body
-    const updatedPerson = {
-        name: body.name,
-        number: body.number
-    }
+  const id = req.params.id
+  const body = req.body
+  const updatedPerson = {
+    name: body.name,
+    number: body.number
+  }
 
-    Person.findByIdAndUpdate(id, updatedPerson, {new:true})
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(err => next(err))
+  Person.findByIdAndUpdate(id, updatedPerson, { new:true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(err => next(err))
 }
 
-morgan.token('body',function (req,res) {return JSON.stringify(req.body)})
+morgan.token('body',function (req,) {return JSON.stringify(req.body)})
 
 app.get('/api/persons',handleGetPersons)
 app.get('/info',handleInfo)
-app.get("/api/persons/:id", handleGetNoteId)
+app.get('/api/persons/:id', handleGetNoteId)
 app.delete('/api/persons/:id',handleDeletePerson)
 app.post('/api/persons' ,handlePostPerson)
 app.put('/api/persons/:id',handleUpdatePerson)
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err.message)
-    if (err.name === 'CastError') {
-        return res.status(400).send({error:"malformatted id"})
-    } else if (err.name === 'ValidationError') {
-        return res.status(400).send({error: err.message})
-    }
-    next(err)
+  console.log(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error:'malformatted id' })
+  } else if (err.name === 'ValidationError') {
+    return res.status(400).send({ error: err.message })
+  }
+  next(err)
 }
 
 app.use(errorHandler)
--- models/person.js
+++ models/person.js
@@ -4,25 +4,25 @@ const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 console.log(`conecting to ${url}`)
-mongoose.connect(url, {useCreateIndex:true, useFindAndModify:false, useNewUrlParser:true, useUnifiedTopology:true})
-    .then(() => {
-        console.log('conected to MongoDB')
-    })
-    .catch(error => {
-        console.log(`error conecting to MongoDB ${error.message}`)
-    })
+mongoose.connect(url, { useCreateIndex:true, useFindAndModify:false, useNewUrlParser:true, useUnifiedTopology:true })
+  .then(() => {
+    console.log('conected to MongoDB')
+  })
+  .catch(error => {
+    console.log(`error conecting to MongoDB ${error.message}`)
+  })
 
 const personSchema = mongoose.Schema({
-    name: { type: String, required: true, unique: true, minLength: 3 },
-    number: {type: String, require: true, minLength: 8}
+  name: { type: String, required: true, unique: true, minLength: 3 },
+  number: { type: String, require: true, minLength: 8 }
 })
 
 personSchema.set('toJSON',{
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 personSchema.plugin(uniqueValidator)
